8f0d293e1c10d38450af27531baf9755dd939f37
Nazmul Hasan <nazmul.tex99@gmail.com>
2023-09-29 23:32:04 +0600

    live test

--- lib/main.dart
+++ lib/main.dart
@@ -35,12 +35,54 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   
-  
+  List < String> imageUrl = [
+    'https://via.placeholder.com/150',
+    'https://via.placeholder.com/150',
+   'https://via.placeholder.com/150',
+    'https://via.placeholder.com/150',
+     'https://via.placeholder.com/150',
+    'https://via.placeholder.com/150',
+     'https://via.placeholder.com/150',
+    'https://via.placeholder.com/150',
+     'https://via.placeholder.com/150',
+    'https://via.placeholder.com/150',
+
+
+
+  ];
+
+
+
   @override
   Widget build(BuildContext context) {
+      final orientation =MediaQuery.of(context).orientation;
+        final portraitOrientation = orientation==Orientation.portrait;
+
    return Scaffold(
-   
-       );
+    appBar: AppBar(title: const Text("News feed"),
+    elevation: 2,
+    ),
+    body: portraitOrientation?  ListView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: imageUrl.length,
+        itemBuilder: (context, index){
+          return  Card(
+            child: Image.network(imageUrl[index],
+            width: 150,
+            height: 150,),
+          );
+      }
+      )
+      : GridView.builder(
+        itemCount: imageUrl.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2),
+           itemBuilder: (context ,index){
+            return Card(
+              child: Image.network(imageUrl[index]),
+            );
+           })
+     );
      
   }
 
